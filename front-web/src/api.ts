78b7ac1485823889b78e9d7b03ff16d291c1f216
Romulo Paiva <romulo.paiva@sinqia.com.br>
2021-01-10 00:47:47 -0300

    Search order location

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -5,3 +5,9 @@ const API_URL = 'http://localhost:8080';
 export function fetchProducts() {
     return axios.get(`${API_URL}/products`)
 }
+
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+
+export function fetchLocalMapBox(local: string) {
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
+}

a63aa711f23fe93f3f75100e15870d3a432fb3a3
Romulo Paiva <romulo.paiva@sinqia.com.br>
2021-01-10 12:53:10 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const API_URL = 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export function fetchProducts() {
     return axios.get(`${API_URL}/products`)
